2dab856eca8d5cb1f069d085f5c926212a3ee8f1
Rishijangir566 <rishijangirjr@gmail.com>
2025-06-11 16:35:35 +0530

    routing

---
+++ src/First.jsx
@@ -0,0 +1,15 @@
+// import Header from './user/component/Header'
+import { Outlet } from 'react-router-dom'
+// import Footer from './user/component/Footer'
+
+function First() {
+    return (
+        <>
+            {/* <Header /> */}
+            <Outlet />
+            {/* <Footer /> */}
+        </>
+    )
+}
+
+export default First
